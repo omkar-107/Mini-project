c7d393feccb73204483694767a793be65eead382
omkar <salunkheomkar107@gmail.com>
2024-02-05 13:13:37 +0530

    Initial Commit

---
+++ Mini-project/Mini_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mini_projectApp.swift
+//  Mini-project
+//
+//  Created by mini project on 05/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Mini_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
