10abc5f75dfa6a168eac02a5d2c97e4d4ebc217d
omkar <salunkheomkar107@gmail.com>
2024-02-13 22:16:11 +0530

    integrete

---
+++ TaskEntity+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  TaskEntity+CoreDataClass.swift
+//  Mini-project
+//
+//  Created by mini project on 13/02/24.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(TaskEntity)
+public class TaskEntity: NSManagedObject {
+
+}
